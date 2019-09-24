d05e8dcffeacf9ff0552bd687a26ca79e8efd424
edean78 <edean78@gmail.com>
2019-09-24 16:17:54 -0400

    Created functions to display the question and answers. Createdsome additional css to answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
-// Ready the document
+//Ready the doucment
 $(document).ready(function () {
+
     // create the trivia questions, choices, and answers.
-    var questions = [
+    var triviaQuestions = [
         {
             question: "What Running Back is the all-time leading rusher at the Univsersity of Georgia?",
             choices: ["Nick Chubb", "Todd Gurley", "Herschel Walker", "Garrison Hurst", "Rodney Hampton"],
@@ -12,21 +13,21 @@ $(document).ready(function () {
             question: "What team did the University of Georgia beat to win a National Championship in 1980?",
             choices: ["Southern California (USC)", "Notre Dame", "Alabama", "Clemson", "Miami"],
             correctAnswer: "Notre Dame"
-        }
+        },
 
-    {
+        {
             question: "On September 7th 2019, the University of Georgia named the field at Sanford Stadium after what Hall of Fame coach?",
             choices: ["Wally Butts", "Ray Goff", "Mark Richt", "Vince Dooley", "Kirby Smart"],
             correctAnswer: "Vince Dooley"
-        }
+        },
 
-    {
+        {
             question: "What is the name of the English Bulldog mascot for the University of Georgia?",
             choices: ["UGA", "Hairy Dawg", "Bevo", "Mike", "Cam"],
             correctAnswer: "UGA"
-        }
+        },
 
-    {
+        {
             question: "What 3 time All American Defensive Lineman, was drafted in the first round of the NFL Draft in 2005?",
             choices: ["Richard Seymour", "Jonathan Sullivan", "Charles Grant", "Jarvus Jones", "David Pollack"],
             correctAnswer: "David Pollack"
@@ -42,17 +43,45 @@ $(document).ready(function () {
     var timeCounter = 30;
     // varible to hold the current question
     var curQuestion = 0;
-    var timer = ;
-    
-        // Display question and answers
+    var time ;
 
-        function displayQuestion(array) {
-            for (var i = 0; i < array.length; i++) {
-                
-            }
-        };
-            // Restart game
+    // Display the questions
+    displayQuestion();
 
-            // Create game logic
+    function countDown() {
+        timeCounter --;
 
-        });
+        $("#timer").html(`Time left: ${timeCounter}`);
+    }
+
+    function displayQuestion() {
+        timeCounter = 30;
+        time = setInterval(countDown, 1000)
+        
+        var quest = triviaQuestions[curQuestion].question;
+        var guess = triviaQuestions[curQuestion].choices;
+
+        $("#app").html(`
+            <h4>${quest}</h4>
+            ${displayChoices(guess)}`);
+        $("#timer").html(`Time left: ${timeCounter}`);
+
+    }
+
+    // Display choices/guesses
+    function displayChoices(guess) {
+        result = "";
+
+        for (var i = 0; i < guess.length; i++) {
+            result += `<p class="choice" data-guess="${guess[i]}">${guess[i]}</p>`;
+        }
+
+        return result;
+
+    }
+    // Start game
+
+    // Create game logic
+
+
+});
