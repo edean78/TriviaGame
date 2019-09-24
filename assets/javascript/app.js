2d927f16190fb9b2f6e02446f5c95da1cb8e7f39
edean78 <edean78@gmail.com>
2019-09-24 13:41:08 -0400

    Created an object of the trivia questions, choices, and correct answers.

---
+++ assets/javascript/app.js
@@ -0,0 +1,39 @@
+// Ready the document
+$(document).ready(function(){
+    // create the trivia questions, choices, and answers.
+var question = [
+    {
+        question:"What Running Back is the all-time leading rusher at the Univsersity of Georgia?",
+        choices: ["Nick Chubb", "Todd Gurley", "Herschel Walker", "Garrison Hurst", "Rodney Hampton"],
+        correctAnswer: "Herschel Walker"
+    },
+
+    {
+        question: "What team did the University of Georgia beat to win a National Championship in 1980?",
+        choices: ["Southern California (USC)", "Notre Dame", "Alabama", "Clemson", "Miami"],
+        correctAnswer: "Notre Dame"
+    }
+
+    {
+        question: "On September 7th 2019, the University of Georgia named the field at Sanford Stadium after what Hall of Fame coach?",
+        choices: ["Wally Butts", "Ray Goff", "Mark Richt", "Vince Dooley", "Kirby Smart"],
+        correctAnswer: "Vince Dooley"
+    }
+
+    {
+        question: "What is the name of the English Bulldog mascot for the University of Georgia?",
+        choices: ["UGA", "Hairy Dawg", "Bevo", "Mike", "Cam"],
+        correctAnswer: "UGA"
+    }
+
+    {
+        question: "What 3 time All American Defensive Lineman, was drafted in the first round of the NFL Draft in 2005?",
+        choices: ["Richard Seymour", "Jonathan Sullivan", "Charles Grant", "Jarvus Jones", "David Pollack"],
+        correctAnswer: "David Pollack"
+    }
+
+]
+
+
+    
+});
